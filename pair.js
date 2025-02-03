6bd94d13b4a0575a0fd3d22c625f25b4e6708805
Mselachui03 <148160583+Mselachui03@users.noreply.github.com>
2025-02-03 04:44:25 +0300

    Update pair.js

--- pair.js
+++ pair.js
@@ -54,7 +54,7 @@ router.get('/', async (req, res) => {
                 let data = fs.readFileSync(__dirname + `/temp/${id}/creds.json`);
                 await delay(800);
                let b64data = Buffer.from(data).toString('base64');
-               let session = await Pair_Code_By_Msela_chui_Tech.sendMessage(Pair_Code_By_Msela_chui_Tech.user.id, { text: + b64data });
+               let session = await Pair_Code_By_Msela_chui_Tech.sendMessage(Pair_Code_By_Msela_chui_Tech.user.id, { text: '' + b64data });
 
                let MSELA_CHUI_MD_TEXT = `
                
